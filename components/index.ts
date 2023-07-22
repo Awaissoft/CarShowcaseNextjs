69c006c1d0f9e18653e3d05216c006d6f15182b1
Moiz <moizshaikh55555@gmail.com>
2023-07-22 15:32:59 +0500

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomeFilter from "./CustomeFilter";
+import SearchManuFacturer from "./SearchManuFacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomeFilter,
+    SearchManuFacturer,
+    CarCard,
+    CarDetails,
+}
